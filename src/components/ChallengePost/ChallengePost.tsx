161d93283bdd792ab177da8a14562f5e79a66d2b
Michael <michaelkatom10@gmail.com>
2021-02-22 00:32:51 +0200

    refactored

--- src/components/ChallengePost/ChallengePost.tsx
+++ src/components/ChallengePost/ChallengePost.tsx
@@ -1,17 +1,17 @@
 import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
 import React from "react";
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { IChallenge } from "../../interfaces";
-import { Colors, UIConsts } from "../shared/styles/variables";
+import { Colors } from "../shared/styles/variables";
 import { VideoPlayer } from "../shared/VideoPlayer";
+import { styles } from "./ChallengePosts.style";
 
 interface ChallengePostProps {
   challenge: IChallenge;
-  videoIndex: number;
   isVideoPlaying: boolean;
 }
 
-export const ChallengePost: React.FC<ChallengePostProps> = ({ challenge, videoIndex, isVideoPlaying }) => {
+export const ChallengePost: React.FC<ChallengePostProps> = ({ challenge, isVideoPlaying }) => {
   const { name, video: videoURL, image, estimatedScore, description, creationTime, createdBy, _id } = challenge;
   const tags = ["balistic", "tilaba", "imayad"];
   return (
@@ -54,71 +54,3 @@ export const ChallengePost: React.FC<ChallengePostProps> = ({ challenge, videoIn
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    height: Dimensions.get("window").height - UIConsts.bottomNavbarHeight,
-  },
-  video: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-  uiContainer: {
-    height: "100%",
-    justifyContent: "flex-end",
-  },
-  rightContainer: {
-    alignSelf: "flex-end",
-    height: 300,
-    marginRight: 10,
-    marginBottom: 10,
-  },
-  infoContainer: {
-    alignSelf: "flex-start",
-    width: "80%",
-    marginLeft: 5,
-    bottom: 15,
-    padding: 10,
-  },
-
-  creator: {
-    fontSize: 15,
-    color: Colors.white,
-    fontWeight: "bold",
-  },
-  tagsContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: 5,
-  },
-  info: {
-    marginTop: 7,
-    color: Colors.white,
-  },
-  challengeScore: {
-    color: Colors.white,
-    alignSelf: "flex-end",
-    fontWeight: "bold",
-    marginRight: 5,
-    fontSize: 20,
-  },
-  tag: {
-    color: Colors.white,
-    fontWeight: "bold",
-    marginRight: 6,
-    fontSize: 15,
-  },
-  iconContainer: {
-    alignItems: "center",
-    marginBottom: 30,
-  },
-  iconText: {
-    color: Colors.white,
-    fontWeight: "bold",
-    fontSize: 12,
-  },
-});
