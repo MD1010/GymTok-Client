5f28a1dc4df961267ee1048c08d1d042f41a01d7
Michael <michaelkatom10@gmail.com>
2021-04-01 22:18:29 +0300

    before handling state issue

--- src/components/shared/UserList.tsx
+++ src/components/shared/UserList.tsx
@@ -14,7 +14,7 @@ interface IUserListProps {
 
 const UserWithAvatar: FC<{ user: IUser }> = ({ user: { fullName, username } }) => (
   <View style={{ flexDirection: "row", alignItems: "center" }}>
-    <Avatar source={require("../../../assets/avatar/user.png")} rounded size={45}></Avatar>
+    <Avatar source={require("../../../assets/avatar/user.png")} rounded size={50}></Avatar>
     <View>
       <Text style={styles.userFullName}>{fullName}</Text>
       <Text style={styles.username}>@{username}</Text>
--- src/components/shared/skeletons/UserSkeletons.tsx
+++ src/components/shared/skeletons/UserSkeletons.tsx
@@ -9,11 +9,11 @@ export const UserSkeletons = () => (
   <FlatList
     keyExtractor={(_, i) => i.toString()}
     data={new Array(Math.round(Dimensions.get("window").height / 60))}
-    renderItem={({ index }) => (
+    renderItem={() => (
       <View style={{ height: 50, marginBottom: 15 }}>
         <SvgAnimatedLinearGradient primaryColor={colors.grey1} secondaryColor={colors.grey2}>
           <Rect x="60" y="8" rx="3" ry="3" width="88" height="10" />
-          <Rect x="60" y="26" rx="3" ry="3" width="52" height="8" />
+          <Rect x="60" y="28" rx="3" ry="3" width="52" height="10" />
           <Circle cx="25" cy="25" r="25" />
         </SvgAnimatedLinearGradient>
       </View>
