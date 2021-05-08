177837124cb430d2117407110a7f11c28a2eef8e
Dor Buchris <dor1633@gmail.com>
2021-05-08 13:28:51 +0300

    continue styling

--- src/components/Explore/PopularHashtag.tsx
+++ src/components/Explore/PopularHashtag.tsx
@@ -12,10 +12,18 @@ interface PopularHashtagProps {
 export const PopularHashtag: React.FC<PopularHashtagProps> = ({ hashtag, posts }) => {
     return (
         <View style={styles.hashtagContainer}>
-            <View style={styles.hashtagDetails}>
-                <Text style={styles.hashtagName}>
-                    # {hashtag}
-                </Text>
+            <View style={styles.popularHashtagDetails}>
+                <View style={styles.hashtagDetails}>
+                    <View style={styles.circle}>
+                        <Text style={styles.hashtag}>
+                            #
+                    </Text>
+                    </View>
+                    <Text style={styles.hashtagName}>
+                        {hashtag}
+                    </Text>
+                </View>
+
                 <Text style={styles.postsCount}>
                     {posts.length}
                 </Text>
@@ -31,14 +39,32 @@ const styles = StyleSheet.create({
         position: "relative",
         marginBottom: 30,
     },
-    hashtagDetails: {
+    popularHashtagDetails: {
         marginLeft: 10,
         marginBottom: 5,
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between"
     },
+    hashtagDetails: {
+        flexDirection: "row"
+    },
+    circle: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        borderWidth: 1,
+        textAlign: 'center',
+        borderColor: Colors.darkGrey,
+    },
+    hashtag: {
+        textAlign: "center",
+        textAlignVertical: "center",
+        color: Colors.white,
+        fontSize: 20
+    },
     hashtagName: {
+        marginLeft: 5,
         color: Colors.white,
         fontSize: 20
     },
