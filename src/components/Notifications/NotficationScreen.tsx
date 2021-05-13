ad919e9dc53fc2ea5c79ae66706f4e5cd7b59f62
Michael <michaelkatom10@gmail.com>
2021-05-13 18:35:40 +0300

    rendering fixed

--- src/components/Notifications/NotficationScreen.tsx
+++ src/components/Notifications/NotficationScreen.tsx
@@ -25,100 +25,109 @@ import Ripple from "react-native-material-ripple";
 import Swipeable from "react-native-gesture-handler/Swipeable";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-const Notification = memo(({ notification, userId }: { notification: INotification; userId: string }) => {
-  const { title, sender, date, isRead, data, body, _id } = notification;
-  const [postTime, setPostTime] = useState<string>(calcTimeAgo(date));
-  const navigation = useNavigation();
-  const dispatch = useDispatch();
-  // const {} = useSelector(postsSelector)
-  console.log("render notification", notification._id);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setPostTime(calcTimeAgo(date));
-    }, 1000 * 60);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const openNotification = (notification: INotification) => {
-    navigation.navigate("VideoDisplay", { posts: [notification.data] });
-    dispatch(markNotificationAsRead(notification, userId));
-  };
-
-  const handleDeleteNotification = (notification: INotification) => {
-    dispatch(deleteNotification(notification, userId));
-  };
-
-  const leftActions = (progress, dragX) => {
-    const scale = dragX.interpolate({
-      inputRange: [0, 100],
-      outputRange: [0, 1],
-      extrapolate: "clamp",
-    });
-    return (
-      <View style={styles.leftAction}>
-        <Animated.Text style={[styles.actionText, { transform: [{ scale }] }]}>
-          <Feather name="trash-2" color={Colors.white} size={25} />
-        </Animated.Text>
-      </View>
-    );
-  };
-
-  return (
-    <Swipeable renderLeftActions={leftActions} onSwipeableOpen={() => handleDeleteNotification(notification)}>
-      <TouchableHighlight
-        underlayColor={Colors.darkBlueOpaque}
-        onPress={() => openNotification(notification)}
-        style={{
-          backgroundColor: Colors.black,
-          flexDirection: "row",
-          alignItems: "center",
-          padding: 10,
-          justifyContent: "center",
-        }}
-      >
-        <>
-          <Avatar source={sender.image ? { uri: sender.image } : images.avatar} rounded size={50}></Avatar>
-
-          <View style={{ marginHorizontal: 15 }}>
-            <Text style={!isRead && { fontWeight: "bold" }}>
-              <Text style={styles.username}>{sender.fullName} </Text>
-              <Text style={styles.content}>Challenged you to try his challenge </Text>
-            </Text>
-            <Text style={[styles.content, !isRead && { fontWeight: "bold" }]}>{body}</Text>
-            <Text style={styles.postTime}>{postTime}</Text>
-          </View>
-          {isRead ? null : <View style={styles.notRead}></View>}
-        </>
-      </TouchableHighlight>
-    </Swipeable>
-  );
-});
 const EmptyNotificationsList = () => (
   <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
     <Ionicons name="notifications-sharp" color={Colors.white} size={56} />
     <Text style={{ paddingVertical: 10, fontSize: 16, padding: 15 }}>You have no notifications left</Text>
   </View>
 );
+
+const Notification = memo(
+  ({ notification, userId }: { notification: INotification; userId: string }) => {
+    const { title, sender, date, isRead, data, body, _id } = notification;
+    const [postTime, setPostTime] = useState<string>(calcTimeAgo(date));
+    const navigation = useNavigation();
+    const dispatch = useDispatch();
+    // const {} = useSelector(postsSelector)
+    console.log("render notification", notification._id);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setPostTime(calcTimeAgo(date));
+      }, 1000 * 60);
+
+      return () => clearInterval(interval);
+    }, []);
+
+    const openNotification = (notification: INotification) => {
+      navigation.navigate("VideoDisplay", { posts: [notification.data] });
+      dispatch(markNotificationAsRead(notification, userId));
+    };
+
+    const handleDeleteNotification = (notification: INotification) => {
+      dispatch(deleteNotification(notification, userId));
+    };
+
+    const leftActions = (progress, dragX) => {
+      const scale = dragX.interpolate({
+        inputRange: [0, 100],
+        outputRange: [0, 1],
+        extrapolate: "clamp",
+      });
+      return (
+        <View style={styles.leftAction}>
+          <Animated.Text style={[styles.actionText, { transform: [{ scale }] }]}>
+            <Feather name="trash-2" color={Colors.white} size={25} />
+          </Animated.Text>
+        </View>
+      );
+    };
+
+    return (
+      <Swipeable renderLeftActions={leftActions} onSwipeableOpen={() => handleDeleteNotification(notification)}>
+        <TouchableHighlight
+          underlayColor={Colors.darkBlueOpaque}
+          onPress={() => openNotification(notification)}
+          style={{
+            backgroundColor: Colors.black,
+            flexDirection: "row",
+            alignItems: "center",
+            padding: 10,
+            justifyContent: "center",
+          }}
+        >
+          <>
+            <Avatar source={sender.image ? { uri: sender.image } : images.avatar} rounded size={50}></Avatar>
+
+            <View style={{ marginHorizontal: 15 }}>
+              <Text style={!isRead && { fontWeight: "bold" }}>
+                <Text style={styles.username}>{sender.fullName} </Text>
+                <Text style={styles.content}>Challenged you to try his challenge </Text>
+              </Text>
+              <Text style={[styles.content, !isRead && { fontWeight: "bold" }]}>{body}</Text>
+              <Text style={styles.postTime}>{postTime}</Text>
+            </View>
+            {isRead ? null : <View style={styles.notRead}></View>}
+          </>
+        </TouchableHighlight>
+      </Swipeable>
+    );
+  },
+  (prev, next) => {
+    return prev.notification._id === next.notification._id;
+  }
+);
+
 export const NotificationScreen = () => {
-  const { receivedNotifications: notifications, error, isLoading } = useSelector(notificaitonsSelector);
   const loggedUser = useSelector(authSelector).loggedUser;
   const dispatch = useDispatch();
 
-  const renderItem = ({ item, index }) => <Notification key={item._id} notification={item} userId={loggedUser._id} />;
+  const NotificationList = () => {
+    const { receivedNotifications: notifications, error, isLoading } = useSelector(notificaitonsSelector);
+    const keyExtractor = (item: INotification, i) => item._id;
+    const renderItem = ({ item, index }) => <Notification key={item._id} notification={item} userId={loggedUser._id} />;
 
-  const NotificationList = () => (
-    <View style={{ marginVertical: 15 }}>
+    return notifications.length ? (
       <FlatList renderItem={renderItem} keyExtractor={keyExtractor} data={notifications}></FlatList>
-    </View>
-  );
+    ) : (
+      <EmptyNotificationsList />
+    );
+  };
 
   const clearNotifications = () => {
     dispatch(deleteUserNotifications(loggedUser._id));
   };
 
-  const keyExtractor = useCallback((item: INotification, i) => item._id, []);
   if (loggedUser) {
     return (
       <>
@@ -145,7 +154,9 @@ export const NotificationScreen = () => {
           </View>
         </View>
         <Divider style={{ backgroundColor: Colors.lightGrey }} />
-        {notifications.length ? <NotificationList /> : <EmptyNotificationsList />}
+        <View style={{ marginVertical: 15, flex: 1 }}>
+          <NotificationList />
+        </View>
       </>
     );
   } else {
