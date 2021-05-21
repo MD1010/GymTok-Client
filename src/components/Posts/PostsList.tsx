2ba29b9e173ca617396899cfc189b761cc88ee99
Michael <michaelkatom10@gmail.com>
2021-05-21 23:29:11 +0300

    fix bug!!

--- src/components/Posts/PostsList.tsx
+++ src/components/Posts/PostsList.tsx
@@ -89,7 +89,7 @@ export const PostsList: React.FC<PostsListProps> = memo(({ isFeed, currentPosts,
     if (loggedUser !== undefined) {
       console.log("loading...");
       setIsLoading(true);
-      isEmpty(posts) && getPosts();
+      isEmpty(posts) && isEmpty(currentPosts) && getPosts();
     }
 
     // getPosts();
@@ -157,18 +157,8 @@ export const PostsList: React.FC<PostsListProps> = memo(({ isFeed, currentPosts,
     [isFeed, bottomTabsHeight]
   );
   const config = useRef<ViewabilityConfig>({
-    itemVisiblePercentThreshold: 90,
-    // minimumViewTime: 150,
+    itemVisiblePercentThreshold: 100,
   });
-
-  const itemLayout = useCallback(
-    (_, index) => ({
-      length: viewHeight,
-      offset: viewHeight * index,
-      index,
-    }),
-    []
-  );
 
   const Footer = () => {
     if (posts.length) {
@@ -236,7 +226,7 @@ export const PostsList: React.FC<PostsListProps> = memo(({ isFeed, currentPosts,
           disableIntervalMomentum
           renderItem={renderItem}
           keyExtractor={keyExtractor}
-          getItemLayout={itemLayout}
+          // getItemLayout={itemLayout}
           snapToAlignment={"start"}
           decelerationRate={"fast"}
           ref={(ref) => {
